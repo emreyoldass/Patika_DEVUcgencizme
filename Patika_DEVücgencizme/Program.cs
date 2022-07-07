18de85f66c4e0a95b589ee44329c943b25ba8b94
Emre Yoldaş <53980583+emreyoldass@users.noreply.github.com>
2022-07-07 08:19:30 +0300

    Proje dosyası ekle.

---
+++ Patika_DEVücgencizme/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Patika_DEVücgencizme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int say;
+            Console.WriteLine("Üçgen Oluşturma Konsoluna Hoşgeldiniz");
+            
+            Console.WriteLine("1-) Dik Üçgen (90° - 45° - 45°)");
+            Console.WriteLine("2-) Ters Dik Üçgen (90° - 45° - 45°)");
+            Console.WriteLine("3-) Üçgen (60° - 60° - 60°)");
+            Console.WriteLine("4-) Birleştirilmiş Üçgen (Baş kısmı bulunmayan)");
+            ücgenler ücgen = new ücgenler();
+            restart:
+            Console.WriteLine("Seçmek istediğiniz Üçgen Çeşidi Nedir ?");
+            int secim = int.Parse(Console.ReadLine());
+            if (secim <= 4)
+            {
+
+                switch (secim)
+                {
+                    case 1:
+                        Console.WriteLine("Oluşturmak için bir değer giriniz :");
+                        say = int.Parse(Console.ReadLine());
+                        ücgen.dikücgen(say);
+                        break;
+                    case 2:
+                        Console.WriteLine("Oluşturmak için bir değer giriniz :");
+                        say = int.Parse(Console.ReadLine());
+                        ücgen.tersdikücgen(say);
+                        break;
+                    case 3:
+                        Console.WriteLine("Oluşturmak için  bir değer giriniz :");
+                        say = int.Parse(Console.ReadLine());
+                        ücgen.ücgen(say);
+                        break;
+                    case 4:
+                        Console.WriteLine("Oluşturmak için bir değer giriniz :");
+                        say = int.Parse(Console.ReadLine());
+                        ücgen.birlestirilmisücgen(say);
+                        break;
+                }
+                Console.WriteLine();
+
+            }
+           
+            else
+            {
+                Console.WriteLine("Var olmayan bir üçgen çeşidini seçtiniz. Lütfen tekrar seçim yapın.");
+                goto restart;
+            }
+            Console.WriteLine("Başa dönmek istermisiniz E/Y");
+            string tekrardeneme = Console.ReadLine();
+            if (tekrardeneme.ToLower() == "e")
+            {
+                goto restart;
+            }
+        }
+    }
+}
---
+++ Patika_DEVücgencizme/ücgenler.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patika_DEVücgencizme
+{
+    internal class ücgenler
+    {
+        public void dikücgen(int say)
+        {
+            //dik üçgen (90°- 45° - 45°)
+            for (int i = 0; i < say; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();            
+            }
+        }
+        public void tersdikücgen(int say)
+        {
+            for (int i = 0; i < say; i++)
+            {
+                for (int j = i; j < say-1; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+        public void ücgen(int say)
+        {
+            //Düz üçgen (60°- 60° - 60°) 
+            for (int i = 0; i < say; i++)
+            {
+                for (int j = i; j < say; j++)
+                {
+                    Console.Write(" ");
+
+                }
+                for (int x = 0; x < i; x++)
+                {
+                    Console.Write("*");
+                    Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public void birlestirilmisücgen(int say)
+        {
+            for (int i = 0; i < say; i++)
+            {
+                
+                for (int a = i; a < say; a++)
+                {
+                    Console.Write(" ");
+                   
+                  
+                }
+                for (int x = 0; x < i; x++)
+                {
+                    Console.Write("*");
+                    Console.Write("");
+                }
+                for (int t = 0; t < say; t++)
+                {
+                    Console.Write("");
+                }
+                for (int q = 0; q < i; q++)
+                {
+                    Console.Write("");
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+    }
+}
+
